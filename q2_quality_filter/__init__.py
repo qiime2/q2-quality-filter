44cd16f4edde47c2b7c00fd8d6ecf5267ce33be1
Greg Caporaso <gregcaporaso@gmail.com>
2017-11-22 12:02:13 -0500

    ENH: adds q_score_joined (#36)

--- q2_quality_filter/__init__.py
+++ q2_quality_filter/__init__.py
@@ -6,11 +6,11 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from ._filter import q_score
+from ._filter import q_score, q_score_joined
 from ._viz_stats import visualize_stats
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
 
-__all__ = ['q_score', 'visualize_stats']
+__all__ = ['q_score', 'q_score_joined', 'visualize_stats']
